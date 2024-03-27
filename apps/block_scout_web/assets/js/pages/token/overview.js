5dd5dc4ab8961f9c525a901281f71d3b31444ffd
anvztor <15998375+anvztor@users.noreply.github.com>
2024-03-27 16:31:40 +0800

    debug: add stub logs

--- apps/block_scout_web/assets/js/pages/token/overview.js
+++ apps/block_scout_web/assets/js/pages/token/overview.js
@@ -2,10 +2,15 @@ import $ from 'jquery'
 import { appendTokenIcon } from '../../lib/token_icon'
 
 if ($('[data-page="token-details"]').length) {
+  console.log("append token icon")
   const $tokenIconContainer = $('#token-icon')
   const chainID = $tokenIconContainer.data('chain-id')
   const addressHash = $tokenIconContainer.data('address-hash')
   const displayTokenIcons = $tokenIconContainer.data('display-token-icons')
+  console.log(chainID)
+  console.log(addressHash)
+  console.log(displayTokenIcons)
+  console.log("append token icon finished")
 
   appendTokenIcon($tokenIconContainer, chainID, addressHash, displayTokenIcons)
 }
